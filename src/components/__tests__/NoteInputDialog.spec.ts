df0f1784c77735681cfcbf90271a6691688a04b9
Ernest Irabor <irabeny89@gmail.com>
2022-12-27 19:38:48 +0100

    feat(test): incomplete unit test

---
+++ src/components/__tests__/NoteInputDialog.spec.ts
@@ -0,0 +1,11 @@
+import { describe, it, expect } from 'vitest'
+
+import { mount } from '@vue/test-utils'
+import NoteInputDialog from '../NoteInputDialog.vue'
+
+// describe('HelloWorld', () => {
+//   it('renders properly', () => {
+//     const wrapper = mount(NoteInputDialog, { props: {  } })
+//     expect(wrapper.text()).toContain('Hello Vitest')
+//   })
+// })
